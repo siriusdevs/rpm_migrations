67710cba27e37616114e6aa0497667a5d08d2921
siriusdevs <tenigin.aa@talantiuspeh.ru>
2023-03-22 14:15:52 +0300

    admin panel stable with all models and inlines

--- hobbies/hobbies_app/admin.py
+++ hobbies/hobbies_app/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Person, Hobby, Language, HobbyCategory, PersonHobby, PersonLanguage
 
-# Register your models here.
+
+class PersonHobby_inline(admin.TabularInline):
+    model = PersonHobby
+    extra = 1
+
+class PersonLanguage_inline(admin.TabularInline):
+    model = PersonLanguage
+    extra = 1
+
+@admin.register(Hobby)
+class HobbyAdmin(admin.ModelAdmin):
+    model = Hobby
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+    model = Language
+
+@admin.register(HobbyCategory)
+class HobbyCategoryAdmin(admin.ModelAdmin):
+    model = HobbyCategory
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    model = Person
+
+    inlines = (
+        PersonHobby_inline,
+        PersonLanguage_inline
+    )
